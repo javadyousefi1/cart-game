7d423164cd5628044fa0565cc5852398a9058fb8
Javad Yousefi <javadyousefi9000@gmail.com>
2024-04-07 00:40:42 +0330

    write first logic of game

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,149 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import FlipHoverEffect from "./components/card/card";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const array = [1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 5, 6, 7, 8];
+  const [list, setList] = useState<number[]>([]);
+  useEffect(() => {
+    function shuffleArray(array) {
+      // Fisher-Yates shuffle algorithm
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+
+    const shuffledArray = shuffleArray(JSON.parse(JSON.stringify(array)));
+    setList(shuffledArray);
+  }, []);
+
+  // Example usage:
+
+  const [selection, setSelction] = useState<{
+    firstSelect: null | number;
+    secondSelect: null | number;
+  }>({
+    firstSelect: null,
+    secondSelect: null,
+  });
+
+  const [foundItem, setFoundItem] = useState<number[]>([]);
+
+  const [closeAll, setCloseAll] = useState(false);
+  const [openAll, setOpenAll] = useState(false);
+
+  const [disableAllButton, setDisableAllButton] = useState(false);
+
+  useEffect(() => {
+    console.log(selection.firstSelect, selection.secondSelect);
+    // console.log(foundItem)
+    if (selection.firstSelect && selection.secondSelect) {
+      setDisableAllButton(true);
+      setTimeout(() => {
+        setDisableAllButton(false);
+      }, 1000);
+      if (selection.firstSelect === selection.secondSelect) {
+        setFoundItem((prev) => [...prev, selection.firstSelect]);
+        setSelction({
+          firstSelect: null,
+          secondSelect: null,
+        });
+        setDisableAllButton(false);
+      }
+
+      if (selection.firstSelect !== selection.secondSelect) {
+        setSelction({
+          firstSelect: null,
+          secondSelect: null,
+        });
+        setTimeout(() => {
+          setCloseAll((prev) => !prev);
+        }, 1000);
+      }
+    }
+  }, [selection]);
+
+  const handleSelection = (newSelect: number): void => {
+    // setSelction(newSelect);
+    if (selection.firstSelect === null) {
+      setSelction({ firstSelect: newSelect, secondSelect: null });
+    } else if (selection.firstSelect && selection.secondSelect === null) {
+      setSelction((prev) => {
+        return { ...prev, secondSelect: newSelect };
+      });
+    }
+  };
+
+  const resetGame = () => {
+    setFoundItem([]);
+    setSelction({
+      firstSelect: null,
+      secondSelect: null,
+    });
+    setDisableAllButton(false);
+    function shuffleArray(array) {
+      // Fisher-Yates shuffle algorithm
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+
+    const shuffledArray = shuffleArray(JSON.parse(JSON.stringify(array)));
+    setList(shuffledArray);
+    setOpenAll((prev) => !prev);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center flex-col gap-y-8  h-screen bg-[#E5C287]">
+      <h1 className="text-lg font-bold"> Javascript Game</h1>
+      <div className="grid grid-cols-4 grid-rows-4 gap-4">
+        {list &&
+          array.map((item, index) => (
+            <FlipHoverEffect
+              key={index}
+              handleSelection={handleSelection}
+              id={item}
+              closeAll={closeAll}
+              foundItem={foundItem}
+              selection={selection}
+              isInteractive={disableAllButton}
+            />
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="h-10">
+        {8 === foundItem.length && (
+          <button
+            type="button"
+            onClick={() => resetGame()}
+            className="font-bold text-[#E8751A] border py-2 px-4 border-[#898121] flex gap-x-2 items-center"
+          >
+            <span>play again</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="15"
+                height="15"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  stroke="#898121"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="M22 12c0 5.52-4.48 10-10 10s-8.89-5.56-8.89-5.56m0 0h4.52m-4.52 0v5M2 12C2 6.48 6.44 2 12 2c6.67 0 10 5.56 10 5.56m0 0v-5m0 5h-4.44"
+                ></path>
+              </svg>
+            </span>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/card/card.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+const SquareComponent: React.FC<{
+  handleSelection: (newSelect: number) => void;
+  id: number;
+  closeAll: boolean;
+  selection: { firstSelect: number | null; secondSelect: number | null };
+  foundItem: number[];
+  isInteractive: boolean;
+}> = ({
+  handleSelection,
+  id,
+  closeAll,
+  foundItem,
+  isInteractive,
+  selection,
+}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  // useEffect(() => {
+  //   console.log("isInteractive", isInteractive);
+  // }, [isInteractive]);
+
+  const handleSquareClick = () => {
+    if (foundItem.includes(id) || isInteractive) {
+      return;
+    }
+    setIsFlipped(!isFlipped);
+  };
+
+  useEffect(() => {
+    if (foundItem.includes(id)) {
+      return;
+    }
+    setIsFlipped(false);
+  }, [closeAll]);
+
+  const handleOnClick = () => {
+    if (foundItem.includes(id) || isInteractive) return;
+    handleSelection(id);
+  };
+
+  return (
+    <div style={{ width: 60, height: 60 }} onClick={() => handleOnClick()}>
+      <motion.div
+        style={{
+          width: "60px",
+          height: "60px",
+          backgroundColor: isFlipped ? "#898121" : "#E8751A",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          position: "relative",
+          overflow: "hidden",
+          cursor: "pointer",
+        }}
+        animate={{ rotateY: isFlipped ? 180 : 0 }}
+        transition={{ duration: 0.5 }}
+        onClick={handleSquareClick}
+      >
+        <motion.div
+          style={{
+            position: "absolute",
+            width: "60px",
+            height: "60px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backfaceVisibility: "hidden",
+            scaleX: isFlipped ? -1 : 1,
+          }}
+        >
+          <div>
+            {isFlipped ? (
+              <div>{id}</div>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M17 18.43h-4l-4.45 2.96A.997.997 0 0 1 7 20.56v-2.13c-3 0-5-2-5-5v-6c0-3 2-5 5-5h10c3 0 5 2 5 5v6c0 3-2 5-5 5Z"
+                  stroke="#FDA403"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+                <path
+                  d="M12 11.36v-.21c0-.68.42-1.04.84-1.33.41-.28.82-.64.82-1.3 0-.92-.74-1.66-1.66-1.66-.92 0-1.66.74-1.66 1.66M11.995 13.75h.01"
+                  stroke="#FDA403"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            )}
+          </div>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default SquareComponent;
